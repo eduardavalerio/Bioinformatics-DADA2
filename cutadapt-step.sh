a1cdc297c0d523969790b16ce73fa586aa2ec033
Eduarda Valério de Jesus <132959155+eduardavalerio@users.noreply.github.com>
2025-05-02 10:49:28 -0300

    Update cutadapt-step.sh

--- cutadapt-step.sh
+++ cutadapt-step.sh
@@ -10,9 +10,18 @@ cutadapt --version
 # Should show the cutadapt version number 
 # 5.0
 
-cutadapt -a AAACTCGTGCCAGCCACC -A GGGTATCTAATCCCAGTTTG -o lib1_1_trimmed.fastq -p lib1_2_trimmed.fastq --minimum-length=130 --quality-cutoff=30 --max-n=0 lib1_1.fq.gz lib1_2.fq.gz
+# To demultiplex and remove adapters using cutadapt I'm gonna use the NGSfilter, the same as I use in OBITools. 
+# Convert NGSfilter for cutadapt accepted file
+# Extract SampleID and first barcode (e.g., 'aaaaaaaa' from 'aaaaaaaa:aaaaaaaa')
+awk '{print $2 "," $3}' ngsfilter_teleo2_lib1.txt | cut -d ':' -f1 > barcodes_cutadapt.csv
+# output barcodes_cutadapt.csv
+# 1ALV231.1S,aaaaaaaa
+# 2ALV231.2S,aaaaaccc
+# 3ALV231.3S,gcaatttt
+# 4ALV231.1P,ggacaaac
 
-# Demultiplex step
+cutadapt -g ^file:barcodes.csv -G ^GGGTATCTAATCCCAGTTTG --front AAACTCGTGCCAGCCACC --pair-adapters --minimum-length=100 --discard-untrimmed --quality-cutoff=30 --max-n=0 -o lib1_R1.fastq -p lib1_R2.fastq input_R1.fq.gz input_R2.fq.gz
+
 
 # After this step -> DADA2 R pipeline
 
