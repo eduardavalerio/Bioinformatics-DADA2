1ca2d78200b36c2299fab977e404294a62e3e2c2
Eduarda Valério de Jesus <132959155+eduardavalerio@users.noreply.github.com>
2025-07-21 08:19:09 -0300

    Update cutadapt-step.sh

--- cutadapt-step.sh
+++ cutadapt-step.sh
@@ -10,8 +10,8 @@ cutadapt --version
 # Should show the cutadapt version number 
 # 5.0
 
-# cutadapt -e 1 -m 1 -g file:barcode.fasta -o output_r1.fastq.gz -p output_r2.fastq.gz input_1.fq.gz input_2.fq.gz
-cutadapt -e 1 -m 1 -g file:barcode.fasta -o lib1_R1.fastq.gz -p lib1_R2.fastq.gz EV_Lib2_EKDL240035960-1A_22F7VJLT4_L4_1.fq.gz EV_Lib2_EKDL240035960-1A_22F7VJLT4_L4_2.fq.gz
+cutadapt -g CACTCTTTCCCTACACGACGCTCTTCCGATCTTAGARTCTTTGAACGCAAATGGC -G GTGACTGGAGGTTCAGACGTGTGCTCTTCCGATCTGCTTATTAATATGCTTAAATTCA 
+-o AI-23-B10_S76_L001_R1_001.trimmed.fastq -p AI-23-B10_S76_L001_R2_001.trimmed.fastq AI-23-B10_S76_L001_R1_001.fastq AI-23-B10_S76_L001_R2_001.fastq --discard-untrimmed  -e 0.12
 
 # -e 1 = at least one mismatch in barcode
 # -m 1 = remove reads that the lenght is zero. 
