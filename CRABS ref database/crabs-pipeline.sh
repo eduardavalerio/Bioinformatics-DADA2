f0772c9123aff264cbc62585c584e7cf7ea886ca
Eduarda Valério de Jesus <132959155+eduardavalerio@users.noreply.github.com>
2025-07-21 14:47:58 -0300

    Update crabs-pipeline.sh

--- CRABS ref database/crabs-pipeline.sh
+++ CRABS ref database/crabs-pipeline.sh
@@ -1,7 +1,7 @@
 # Create virtual environment for CRABS and dowload all packages required 
 conda create -n crabs-env
 conda activate crabs-env
-# Install CRABS
+# Install CRABS (v.1.9.0.)
 git clone https://github.com/gjeunen/reference_database_creator.git
 # Install python packages 
 pip install requests
@@ -21,13 +21,42 @@ conda install bioconda::fasttree
 crabs -h
 
 # Before start check if you're in the right paste to save all the files 
-# I'm gonna use the MitoFish database and NCBI 
+# I'm gonna use the MitoFish and NCBI databases
 
 # STEP 1 - DOWNLOAD SEQUENCE DATA FROM ONLINE REPOSITORIES AND TAXONOMY FROM NCBI
+# Reference sequences
 crabs --download-mitofish --output mitofish.fasta
-
+crabs --download-ncbi --query '("Chondrichthyes"[Organism] OR "Dipnomorpha"[Organism] OR "Actinopterygii"[Organism] OR "Myxini"[Organism] 
+OR "Hyperoartia"[Organism] OR "Coelacanthimorpha"[Organism] OR Fish[All Fields]) AND 12S[All Fields]' --output ncbi_fish12s.fasta 
+--email eduarda.jesus@usp.br --database nucleotide
+# Taxonomy from NCBI
 crabs --download-taxonomy --exclude 'acc2taxid, taxdump'
 crabs --download-taxonomy --output ncbi_taxonomy #ncbi_taxonomy is a subdirectory that I crated -> mkdir ncbi_taxonomy 
+
+# STEP 2 - IMPORT DOWNLOADED DATA INTO CRABS FORMAT
+# mitofish
+crabs --import --import-format mitofish --input mitofish.fasta --names ncbi_taxonomy/names.dmp --nodes ncbi_taxonomy/nodes.dmp
+--acc2tax ncbi_taxonomy/nucl_gb.accession2taxid --output crabs_mitofish.txt --ranks 'superkingdom;phylum;class;order;family;genus;species'
+# ncbi
+crabs --import --import-format ncbi --input nci.fasta --names ncbi_taxonomy/names.dmp --nodes ncbi_taxonomy/nodes.dmp
+--acc2tax ncbi_taxonomy/nucl_gb.accession2taxid --output crabs_ncbi.txt --ranks 'superkingdom;phylum;class;order;family;genus;species'
+
+# STEP 3 - MERGE DOWLOADED SEQUENCES
+# This step is just when sequence data from multiple online repositories are downloaded. After import the data in crabs format step.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
